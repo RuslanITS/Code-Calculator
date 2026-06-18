b69b8263b352e8ec38e285b611d732f4fadd931b
Ruslan <rcernov442@gmail.com>
2026-06-18 18:38:40 +0600

    Removed the unnecessary parts.

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { createRoot } from 'react-dom/client'
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { store } from "./app/store.ts";
@@ -8,12 +7,9 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
-    <BrowserRouter>
       <App />
-
       <ToastContainer
         position="bottom-right"
       />
-    </BrowserRouter>
   </Provider>
 )
